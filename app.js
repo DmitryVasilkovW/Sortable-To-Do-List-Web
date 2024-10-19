79dea40e3d1a00bf472c17329dabb37e438b194f
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-10-19 17:52:25 +0300

    fix: opened menu as a default

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 let currentTab = 0;
 let tags = [];
 
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.querySelector('.main-content');
+
+    const isSidebarOpen = localStorage.getItem('sidebarOpen') === 'true';
+
+    if (isSidebarOpen) {
+        sidebar.classList.add('open');
+        mainContent.style.marginLeft = '250px';
+    } else {
+        sidebar.classList.remove('open');
+        mainContent.style.marginLeft = '0';
+    }
+});
+
 function switchTab(tabIndex) {
     document.getElementById(`list-${currentTab}`).style.display = 'none';
     currentTab = tabIndex;
@@ -20,12 +35,17 @@ function addTask(listIndex) {
 
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('open');
     const mainContent = document.querySelector('.main-content');
+
+    // Тогглим (переключаем) состояние меню
+    sidebar.classList.toggle('open');
+
     if (sidebar.classList.contains('open')) {
         mainContent.style.marginLeft = '250px';
+        localStorage.setItem('sidebarOpen', 'true');
     } else {
         mainContent.style.marginLeft = '0';
+        localStorage.setItem('sidebarOpen', 'false');
     }
 }
 
