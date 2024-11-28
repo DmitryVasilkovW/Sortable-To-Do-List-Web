4f930d83df5f901e0e05574a95b9055fe7e28010
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-11-29 00:40:39 +0300

    fix: fixed bug with deleting a task where it was still in the array

--- src/app/task/menu/delete.js
+++ src/app/task/menu/delete.js
@@ -1,6 +1,6 @@
 function deleteTask(taskElement, tabIndex) {
-    taskElement.remove();
-
     const taskIndex = Array.from(taskElement.parentNode.children).indexOf(taskElement);
     todoListsData[tabIndex].splice(taskIndex, 1);
+    
+    taskElement.remove();
 }
